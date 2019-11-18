036478bfee829f7415ce2e25006e5e07785d7849
Brandon Ruhl <brandon.ruhl@cbsinteractive.com>
2019-11-18 15:33:41 -0800

    adding .gitignore and remove unnecessary requirements

--- setup.py
+++ setup.py
@@ -24,15 +24,6 @@ setuptools.setup(
         },
 
     install_requires = [
-        'boundary_layer',
-        'enum34>=1.1.6,<2.0',
-        'semver>=2.7.0,<3.0',
-        'jsonschema>=2.6.0,<3.0',
-        'jinja2>=2.8.1,<3.0',
-        'pyyaml>=4.2b1',
-        'marshmallow>=2.13.6,<3.0',
-        'networkx>=2.1,<2.3',
-        'xmltodict>=0.11.0,<1.0',
-        'six>=1.11.0,<2.0',
+        'boundary_layer'
     ]
 )
